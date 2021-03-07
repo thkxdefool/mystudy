4e5566968b072e3bc8c8a15b98c85787ebe0556e
lsl666 <574115360@qq.com>
2021-03-07 17:35:42 +0800

    first modify

--- src/main/java/com/lsl/mystudy/MystudyApplication.java
+++ src/main/java/com/lsl/mystudy/MystudyApplication.java
@@ -2,13 +2,20 @@ package com.lsl.mystudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class MystudyApplication {
 
+    @RequestMapping("/")
+    String index() {
+        return "hello spring boota";
+    }
     public static void main(String[] args) {
         SpringApplication.run(MystudyApplication.class, args);
-        System.out.println("aa");
+
     }
 
 }
