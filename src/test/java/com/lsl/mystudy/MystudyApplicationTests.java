5d5eaf4080e528bb7e9a483cb7fdc36e844dc4df
lsl666 <574115360@qq.com>
2021-03-07 15:38:00 +0800

    init

---
+++ src/test/java/com/lsl/mystudy/MystudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsl.mystudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MystudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
