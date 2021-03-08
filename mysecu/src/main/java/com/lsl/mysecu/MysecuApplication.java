a90f28ab3781ab22ba8b7f731b9720b34f5aa9c9
lsl666 <574115360@qq.com>
2021-03-08 18:52:51 +0800

    first modify

---
+++ mysecu/src/main/java/com/lsl/mysecu/MysecuApplication.java
@@ -0,0 +1,13 @@
+package com.lsl.mysecu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysecuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysecuApplication.class, args);
+    }
+
+}
--- src/main/java/com/lsl/mystudy/MystudyApplication.java
+++ src/main/java/com/lsl/mystudy/MystudyApplication.java
@@ -16,6 +16,7 @@ public class MystudyApplication {
     public static void main(String[] args) {
         SpringApplication.run(MystudyApplication.class, args);
 
+
     }
 
 }
